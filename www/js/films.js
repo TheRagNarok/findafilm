173e16705c4db6b9b79e46b242dd3fc094e0e67a
TheRagNarok <bensmith222@hotmail.co.uk>
2014-05-06 18:26:29 +0100

    ractive update added

--- www/js/films.js
+++ www/js/films.js
@@ -6,9 +6,9 @@ var items = new Array();
 
 
 var films = [
-  { genreA:'Mystery', genreB:'Adventure', title: 'Todo list page', year: '2014', fave: 'on watch', info: 'todo.html' },
-  { genreA:'Romance', genreB:'Comedy',title: 'Search page', year: '2014',fave: 'on watch', info: 'search.html' },
-   { genreA:'Action', genreB:'Adventure',title: 'Harry Potter and the Philosophers Stone', year: '2001', fave: 'z', info: 'search.html'},
+    { genreA:'Mystery', genreB:'Adventure', title: 'Todo list page', year: '2014', fave: 'on watch', info: 'todo.html' },
+    { genreA:'Romance', genreB:'Comedy',title: 'Search page', year: '2014',fave: 'on watch', info: 'search.html' },
+    { genreA:'Action', genreB:'Adventure',title: 'Harry Potter and the Philosophers Stone', year: '2001', fave: 'z', info: 'search.html'},
     { genreA:'Musical', genreB:'Thriller',title: 'Harry Potter and the Chamber of Secrets', year: '2002', fave: 'on watch', info: 'search.html'},
     { genreA:'Western', genreB:'Comedy',title: 'Harry Potter and the Prisoner of Azkaban', year: '2004',fave: 'z', info: 'search.html'},
     { genreA:'Science Fiction', genreB:'Musical',title: 'Harry Potter and the Goblet of Fire', year: '2005', fave: 'on watch', info: 'search.html'},
@@ -21,8 +21,8 @@ var films = [
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-	alert("page loaded");
-	<!-----------------------------------Storage -------------------------------------------------------------->  
+	alert("onDeviceReady Called");
+<!-----------------------------------Storage -------------------------------------------------------------->  
     // If app is being run for the first time, load the default list of films
 if (window.localStorage.getItem("first") === null )
  {
