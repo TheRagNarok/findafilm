214b8b4571bc32db839effcef2351274d4a1bbda
TheRagNarok <bensmith222@hotmail.co.uk>
2014-05-14 13:08:44 +0100

    re-added deviceready

--- www/js/films.js
+++ www/js/films.js
@@ -6,16 +6,16 @@ var items = new Array();
 
 
 var films = [
-    { genreA:'Mystery', genreB:'Adventure', title: 'Todo list page', year: '2014', fave: 'on watch', info: 'todo.html' },
-    { genreA:'Romance', genreB:'Comedy',title: 'Search page', year: '2014',fave: 'on watch', info: 'search.html' },
-    { genreA:'Action', genreB:'Adventure',title: 'Harry Potter and the Philosophers Stone', year: '2001', fave: 'z', info: 'search.html'},
-    { genreA:'Musical', genreB:'Thriller',title: 'Harry Potter and the Chamber of Secrets', year: '2002', fave: 'on watch', info: 'search.html'},
-    { genreA:'Western', genreB:'Comedy',title: 'Harry Potter and the Prisoner of Azkaban', year: '2004',fave: 'z', info: 'search.html'},
-    { genreA:'Science Fiction', genreB:'Musical',title: 'Harry Potter and the Goblet of Fire', year: '2005', fave: 'on watch', info: 'search.html'},
-    { genreA:'Thriller', genreB:'Historical',title: 'Harry Potter and the Order of the Phoenix', year: '2007', fave: 'z', info: 'search.html'},
-    { genreA:'Horror', genreB:'Fantasy',title: 'Harry Potter and the Half-Blood Prince', year: '2009', fave: 'on watch', info: 'search.html'},
-    { genreA:'Western', genreB:'Action',title: 'Harry Potter and the Deathly Hallows – Part 1', year: '2010', fave: 'on watch', info: 'search.html'},
-    { genreA:'Horror', genreB:'Mystery',title: 'Harry Potter and the Deathly Hallows – Part 2', year: '2011', fave: 'z', info: 'search.html'}
+    { genreA:'Mystery', genreB:'Adventure', title: 'Todo list page', year: '2014', fave: 'on watch' },
+    { genreA:'Romance', genreB:'Comedy',title: 'Search page', year: '2014',fave: 'on watch' },
+    { genreA:'Action', genreB:'Adventure',title: 'Harry Potter and the Philosophers Stone', year: '2001', fave: 'z'},
+    { genreA:'Musical', genreB:'Thriller',title: 'Harry Potter and the Chamber of Secrets', year: '2002', fave: 'on watch'},
+    { genreA:'Western', genreB:'Comedy',title: 'Harry Potter and the Prisoner of Azkaban', year: '2004',fave: 'z'},
+    { genreA:'Science Fiction', genreB:'Musical',title: 'Harry Potter and the Goblet of Fire', year: '2005', fave: 'on watch'},
+    { genreA:'Thriller', genreB:'Historical',title: 'Harry Potter and the Order of the Phoenix', year: '2007', fave: 'z'},
+    { genreA:'Horror', genreB:'Fantasy',title: 'Harry Potter and the Half-Blood Prince', year: '2009', fave: 'on watch'},
+    { genreA:'Western', genreB:'Action',title: 'Harry Potter and the Deathly Hallows – Part 1', year: '2010', fave: 'on watch'},
+    { genreA:'Horror', genreB:'Mystery',title: 'Harry Potter and the Deathly Hallows – Part 2', year: '2011', fave: 'z'}
 ];
 
 document.addEventListener("deviceready", onDeviceReady, false);
